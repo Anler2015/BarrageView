0bc54513177f8409df3913088534152bbd92bc75
Anler <hippogjh7758258@qq.com>
2015-10-27 22:32:33 +0800

    Change implementation

--- app/src/main/java/com/anler/barrage/barrageview/BarrageView.java
+++ app/src/main/java/com/anler/barrage/barrageview/BarrageView.java
@@ -2,8 +2,8 @@ package com.anler.barrage.barrageview;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.graphics.Rect;
 import android.text.TextPaint;
 import android.util.AttributeSet;
@@ -12,6 +12,8 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
+
+import java.util.LinkedList;
 import java.util.Random;
 
 /**
@@ -25,13 +27,15 @@ public class BarrageView extends View {
         bottomOfScreen
     }
     private TextPaint txtPaint;
-    private  String txt;
     private int screenHeight;
     private int screenWidth;
     private int x,y = 0;
+    private LinkedList<Point> pos = new LinkedList();
+    private LinkedList<String> txts = new LinkedList();
     private  int speed = 4;
   //  private Paint.FontMetrics mFontMetrics;// 文本测量对象
     private int txtSize = 30;
+    private showMode mShowMode = showMode.topOfScreen;
 
     private Thread rollThread;
 
@@ -47,18 +51,17 @@ public class BarrageView extends View {
 
     private void init()
     {
-        txtPaint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
+        txtPaint = new TextPaint(Paint.ANTI_ALIAS_FLAG | Paint.DITHER_FLAG);
         txtPaint.setTextSize(txtSize);
-        //mFontMetrics = txtPaint.getFontMetrics();
         Rect rect = new Rect();
         getWindowVisibleDisplayFrame(rect);
         screenWidth = rect.width();
         screenHeight = rect.height();
-        x = screenWidth;
+
     }
 
 
-    @Override
+/*    @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
         screenHeight = h;
@@ -113,33 +116,42 @@ public class BarrageView extends View {
             }
         }
         return result;
-    }
+    }*/
 
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawText(txt, x, y, txtPaint);
-        canvas.drawLine(0,screenHeight/2,screenWidth,screenHeight/2,new Paint());
+        for(int i =0;i<pos.size();i++)
+        {
+            canvas.drawText(txts.get(i), pos.get(i).x, pos.get(i).y, txtPaint);
+        }
+        canvas.drawLine(0, screenHeight / 2, screenWidth, screenHeight / 2, new Paint());
         logic();
         invalidate();
+//       if (rollThread == null) {
+//            rollThread = new RollThread(canvas);
+//           rollThread.start();
+//        }
     }
 
 
     private  void logic()
     {
         x -= speed;
-//
-//        if(x < - txtPaint.measureText(txt))
-//        {
-//            ((ViewGroup)this.getParent()).removeView(this);
-//        }
+        for(int i =0;i<pos.size();i++)
+        {
+             pos.get(i).x -=speed;
+            if(pos.get(i).x < -txtPaint.measureText(txts.get(i)))
+            {
+                pos.remove(i);
+                txts.remove(i);
+            }
+            Log.v("gjh",""+txts.size());
+        }
+
     }
 
-    public void setText(String str)
-    {
-        this.txt = str;
-    }
 
     public void setTextSize(int txtSize)
     {
@@ -161,9 +173,9 @@ public class BarrageView extends View {
     {
         this.y = y;
     }
-    public void setShowMode(showMode mode)
+    public int setYShowMode(showMode mode)
     {
-        Log.v("gjh", screenHeight + "");
+
         switch (mode)
         {
             case allScreen:
@@ -176,6 +188,7 @@ public class BarrageView extends View {
                 y = new Random().nextInt(screenHeight/2-txtSize)+screenHeight/2-txtSize;
                 break;
         }
+        return y;
     }
 
 
@@ -191,5 +204,34 @@ public class BarrageView extends View {
                 break;
         }
         return true;
+    }
+
+    public void sendBarrage(String txt)
+    {
+        pos.add(new Point(screenWidth,setYShowMode(mShowMode)));
+        txts.add(txt);
+    }
+
+    class RollThread extends Thread
+    {
+        Canvas canvas;
+        public RollThread(Canvas canvas) {
+            this.canvas = canvas;
+        }
+
+        @Override
+        public void run() {
+            while(true)
+            {
+                logic();
+                try {
+                    Thread.sleep(30);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                postInvalidate();
+            }
+
+        }
     }
 }
--- app/src/main/java/com/anler/barrage/barrageview/MainActivity.java
+++ app/src/main/java/com/anler/barrage/barrageview/MainActivity.java
@@ -16,22 +16,18 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //设置宽高全屏
-        final ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-
+        final ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        final BarrageView barrageView = new BarrageView(MainActivity.this);
+        addContentView(barrageView, lp);
         final Handler handler = new Handler();
         Runnable createBarrageView = new Runnable() {
             @Override
             public void run() {
-                //新建一条弹幕，并设置文字
-                final BarrageView barrageView = new BarrageView(MainActivity.this);
-                barrageView.setText("你好");
-                int color = Color.rgb(random.nextInt(256), random.nextInt(256), random.nextInt(256));
-                barrageView.setTextColor(color);
-           //     barrageView.setY(new Random().nextInt(1280));
-               barrageView.setShowMode(BarrageView.showMode.topOfScreen);
-                addContentView(barrageView,lp);
 
-                //发送下一条消息
+//                int color = Color.rgb(random.nextInt(256), random.nextInt(256), random.nextInt(256));
+//                barrageView.setTextColor(color);
+                barrageView.sendBarrage("你好");
+
                 handler.postDelayed(this, DELAY_TIME);
             }
         };
